e7109cfb1a51bd1348a5a9852a49bddf13458552
Unknown <abdorehab95@gmail.com>
2019-12-11 02:29:17 +0200

    init project

---
+++ lib/scratch.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+
+void main(){
+preformTasks();
+}
+
+void preformTasks(){
+  task1();
+  task2();
+  task3();
+
+}
+
+void task1() {
+  String result='task1 1 data';
+  print('task1 complete');
+
+}
+
+void task2() {
+  String result='task1 2 data';
+  print('task2 complete');
+}
+
+void task3() {
+  String result='task1 3 data';
+  print('task3 complete');
+}
